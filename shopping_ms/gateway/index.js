3054db25102c843e9261f2c5512b1ce02ae73d23
mugiisha <mugishabankz3@gmail.com>
2023-03-04 08:59:47 +0200

    create server per services

---
+++ shopping_ms/gateway/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const proxy = require('express-http-proxy')
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+
+app.use('/customers', proxy('http://localhost:8010'))
+app.use('/shopping', proxy('http://localhost:8030'))
+app.use('/', proxy('http://localhost:8020')) //products
+
+
+app.listen(8040, () => {
+  console.log("gateway running on port 8040");
+});
